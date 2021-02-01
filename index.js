644fd949a994e83014dd25b58aab79bcc30c9774
Vaishnav2606 <bose20009@gmail.com>
2021-02-01 17:29:50 +0530

    added the home.ejs but it only has hello world and set up the controller for the home page and the router for home page

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const port = 8000;
 
 const app = express();
 
+//Using the express Router
+app.use('/', require('./routes'))
 
+//set up the view engine
+app.set("view engine", 'ejs');
+app.set("views", './views');
 
 app.listen(port, (err)=>{
     if(err){
