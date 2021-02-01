1453249e64657f4f70e48efb76befdff50a69514
Vaishnav2606 <bose20009@gmail.com>
2021-02-02 00:35:57 +0530

    New design with header completed. Have to start working on page 1

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use('/', require('./routes'))
 app.set("view engine", 'ejs');
 app.set("views", './views');
 
+//middleware
+app.use(express.static('./assets'))
+
 app.listen(port, (err)=>{
     if(err){
         console.log("error has occured:",err);
